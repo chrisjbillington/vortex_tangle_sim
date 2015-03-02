3f915a9f920ab43134df158100b37f3afad13442
Chris Billington <chrisjbillington@gmail.com>
2015-03-02 16:48:12 +1100

    Testing MPI

--- run.py
+++ run.py
@@ -1,2 +1,8 @@
-import sys
-print(sys.version)
+from __future__ import print_function
+
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+print('hello from MPI process %d!'%rank)
