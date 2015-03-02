19028cec1515b6ae52d44354ad69a5670f106eb5
Chris Billington <chrisjbillington@gmail.com>
2015-03-02 17:12:22 +1100

    Testing if can run on 16 cores

--- job.sh
+++ job.sh
@@ -8,6 +8,6 @@
 #$ -M chrisjbillington@gmail.com
 #$ -m abe
 #$ -N vortex_tangle
-# $ -pe smp 2 #$ -pe orte_adv 9
+#$ -pe orte_adv 16
 export OMP_NUM_THREADS=$NSLOTS
-mpiexec python run.py
+mpirun -np  $NSLOTS python run.py
--- run.py
+++ run.py
@@ -6,4 +6,7 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-print('hello from MPI process {0} of {1}!'.format(rank, size-1))
+x = 'hello from MPI process {0} of {1}!'.format(rank, size-1)
+print(x)
+with open('test.txt','a') as f:
+    f.write(x + '\n')
