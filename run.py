730b3ed29edb82befa0aba9e2a4ffe3d4da00893
Chris Billington <chrisjbillington@gmail.com>
2015-03-02 19:25:56 +1100

    Testing communication between MPI processes

--- run.py
+++ run.py
@@ -6,7 +6,11 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-x = 'hello from MPI process {0} of {1}!'.format(rank, size-1)
-print(x)
-with open('test.txt','a') as f:
-    f.write(x + '\n')
+x = 'hello from MPI process {0} of {1}!\n'.format(rank, size-1)
+
+if rank:
+    x = comm.recv(source=rank-1) + x
+if rank < size-1:
+    comm.send(x, dest=rank+1)
+else:
+    print(x)
