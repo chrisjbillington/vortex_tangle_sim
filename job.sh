b44d3d2894e73788ed7636cfabb154a52adb12af
Chris Billington <chrisjbillington@gmail.com>
2015-03-02 17:05:17 +1100

    Testing processes spread over machines

--- job.sh
+++ job.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #$ -S /bin/sh
-#$ -o stdout
+#$ -o out
 #$ -j y
 #$ -l h_vmem=1G
 #$ -l h_rt=00:01:00
@@ -8,7 +8,6 @@
 #$ -M chrisjbillington@gmail.com
 #$ -m abe
 #$ -N vortex_tangle
-#$ -pe smp 2
-#$ -pe smp 2
+# $ -pe smp 2 #$ -pe orte_adv 9
 export OMP_NUM_THREADS=$NSLOTS
 mpiexec python run.py
--- run.py
+++ run.py
@@ -4,5 +4,6 @@ from mpi4py import MPI
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
+size = comm.Get_size()
 
-print('hello from MPI process %d!'%rank)
+print('hello from MPI process {0} of {1}!'.format(rank, size-1))
