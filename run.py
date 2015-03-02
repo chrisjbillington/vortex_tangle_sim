5b6b8825766a3953e8269bcb5428acc4e769df73
Chris Billington <chrisjbillington@gmail.com>
2015-03-02 20:28:29 +1100

    Processes print what host they are on

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 from __future__ import print_function
 
 from mpi4py import MPI
+import socket
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-x = 'hello from MPI process {0} of {1}!\n'.format(rank, size-1)
+x = 'hello from MPI process {0} of {1}, Running on {2}!\n'.format(rank, size-1, socket.gethostname())
 
 if rank:
     x = comm.recv(source=rank-1) + x
