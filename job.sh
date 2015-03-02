5d4c40c0ad1777a3b6caf4037b8e93230961970b
Chris Billington <chrisjbillington@gmail.com>
2015-03-02 16:56:51 +1100

    Hard coded stdout and stderr filenames to make cleanup easier

--- job.sh
+++ job.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #$ -S /bin/sh
+#$ -o stdout
+#$ -j y
 #$ -l h_vmem=1G
 #$ -l h_rt=00:01:00
 #$ -cwd
@@ -7,6 +9,6 @@
 #$ -m abe
 #$ -N vortex_tangle
 #$ -pe smp 2
-
+#$ -pe smp 2
 export OMP_NUM_THREADS=$NSLOTS
 mpiexec python run.py
