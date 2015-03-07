b8b9041a259ee15f1b3fb47f70318000efb90c90
Chris Billington <chrisjbillington@gmail.com>
2015-03-07 22:57:56 +1100

    Found myself passing a lot of the same data into different functions, so made a class. Class represents a DVR basis over an arbitrary interval.
    Now we'll instantiate several of them so we can test the finite element
    variety of the DVR.

--- DVR_test.py
+++ DVR_test.py
@@ -5,17 +5,15 @@ import numpy as np
 
 def gauss_legendre_points_and_weights(N):
     """Returns the spatial points and weights for the N-point Gauss-Legendre
-    quadrature. These are the spatial points at which all but one of the
-    corresponding DVR basis functions are zero."""
+    quadrature."""
     from scipy.special import p_roots
     points, weights = p_roots(N)
     return points, weights
 
 
-def gauss_lobatto_points_and_weights(N):
+def gauss_lobatto_points_and_weights(N, x_l=-1, x_r=1):
     """Returns the spatial points and weights for the N-point Gauss-Lobatto
-    quadrature. These are the spatial points at which all but one of the
-    corresponding DVR basis functions are zero."""
+    quadrature."""
     from scipy.special import legendre
     points = np.zeros(N)
     weights = np.zeros(N)
@@ -31,10 +29,10 @@ def gauss_lobatto_points_and_weights(N):
     return points, weights
 
 
-def make_DVR_basis(x, w):
-    """Returns a list of numpy Polynomial objects representing the basis functions
-    of the discrete variable representation using the the Gauss quadrature with points
-    x and weights w."""
+def make_DVR_basis_polynomials(x, w):
+    """Returns a list of numpy Polynomial objects representing the basis
+    functions of the discrete variable representation using the the Gauss
+    quadrature with points x and weights w."""
     from numpy.polynomial.polynomial import Polynomial, polyfromroots
     u = []
     for x_i, w_i in zip(x, w):
@@ -46,52 +44,81 @@ def make_DVR_basis(x, w):
     return u
 
 
-def make_DVR_differetial_operator(x, w, order=1):
-    """"Return an len(x) x len(x) array for the matrix representation of the derivative
-    operator of a given order in the DVR basis for points x and weights w.
+class DVRBasis(object):
+    def __init__(self, N, x_l=-1, x_r=1, quadrature='lobatto'):
+        """A class for operations with the N-point discrete variable representation
+        basis for either Gauss-Legendre or Gauss-Lobatto quadrature on an
+        interval [x_l, x_r].
 
-    The matrix elements for the operator are easy to compute, because the
-    integrals for them are exactly given by the quadrature rule."""
-    N = len(x)
-    u = make_DVR_basis(x, w)
-    # Differentiate the basis functions to the given order:
-    dn_u_dxn = [u_i.deriv(order) for u_i in u]
-    dn_dxn = np.zeros((N,N))
-    for i, u_i in enumerate(u):
-        for j, dn_u_j_dxn in enumerate(dn_u_dxn):
-            # Compute one matrix element <u_i | dn_dxn u_j> using the quadrature rule:
-            for x_n, w_n in zip(x, w):
-                dn_dxn[i, j] += w_n*u_i(x_n)*dn_u_j_dxn(x_n)
-    return dn_dxn
+        Attributes:
+            N:     number of quadrature points/basis functions
+            x_l:   left boundary of interval
+            x_r:   right boundary of interval
+            x:     quadrature points
+            w      quadrature weights
+            u      DVR basis polynomials, valid in in the interval [x_l, x_r]
 
+        """
+        self.N = N
+        self.x_l = x_l
+        self.x_r = x_r
+        self.quadrature = quadrature
+        if self.quadrature == 'legendre':
+            self.x, self.w = gauss_lobatto_points_and_weights(N)
+        elif self.quadrature == 'lobatto':
+            self.x, self.w = gauss_lobatto_points_and_weights(N)
+        else:
+            raise ValueError('quadrature must be either \'lobatto\' or \'legendre\'')
+        # Shift the quadrature points from the interval [-1, 1] to [x_l, x_r], and
+        # scale the weights appropriately:
+        self.x = (self.x + 1)*(x_r - x_l)/2 + x_l
+        self.w /= (x_r - x_l)/2
+        # Make the DVR basis functions:
+        self.u = make_DVR_basis_polynomials(self.x, self.w)
 
-def make_DVR_vector(x_dense, psi_dense, x, w):
-    """Takes points x_dense and the values psi_dense of a function at those
-    points, and the quadrature points x and weights w for a DVR basis, and
-    returns an array of length len(x) containing the coefficients for that
-    function's representation in that DVR basis. x_dense should be dense in
-    the domain [-1, 1]. linspace(-1,1,1000) should do it.
+    def make_vector(self, x_dense, psi_dense):
+        """Takes points x_dense and the values psi_dense of a function at those
+        points, and returns an array containing the coefficients for that
+        function's representation in that DVR basis. Only the part of the function
+        in the interval [self.x_l, self.x_r] is used."""
+        psi = np.zeros(self.N)
+        valid = (self.x_l <= x_dense) & (x_dense <= self.x_r)
+        x_dense = x_dense[valid]
+        psi_dense = psi_dense[valid]
+        for i, u_i in enumerate(self.u):
+            psi[i] = np.trapz(psi_dense*u_i(x_dense), x_dense)
+        return psi
 
-    Actually projects the function onto the basis vectors using full integrals
-    rather than assuming the quadrature rule is valid and only using the
-    values of the function at the quadrature points. This ensures we represent
-    it as accurately as possible """
-    psi = np.zeros(len(x))
-    u = make_DVR_basis(x, w)
-    for i, u_i in enumerate(u):
-        psi[i] = np.trapz(psi_dense*u_i(x_dense), x_dense)
-    return psi
+    def interpolate_vector(self, psi, x_dense):
+        """Takes a vector psi in the DVR basis and interpolates the spatial
+        function it represents to the points in the array x_dense"""
+        f = np.zeros(len(x_dense))
+        for psi_i, u_i in zip(psi, self.u):
+            try:
+                f += psi_i*u_i(x_dense)
+            except:
+                import IPython
+                IPython.embed()
+        # Clip outside the valid region:
+        valid = (self.x_l <= x_dense) & (x_dense <= self.x_r)
+        f[~valid] = 0
+        return f
 
+    def differential_operator(self, order=1):
+        """"Return an len(x) x len(x) array for the matrix representation of the derivative
+        operator of a given order in the DVR basis for points x and weights w.
 
-def interpolate_DVR_vector(psi, x, w, x_dense):
-    """Takes a vector psi in the DVR basis with quadrature points x and
-    weights w, and interpolates the spatial function it represents to the
-    points in the array x_dense, which should be in the domain [-1, 1]"""
-    f = np.zeros(len(x_dense))
-    u = make_DVR_basis(x, w)
-    for psi_i, u_i in zip(psi, u):
-        f += psi_i*u_i(x_dense)
-    return f
+        The matrix elements for the operator are easy to compute, because the
+        integrals for them are exactly given by the quadrature rule."""
+        # Differentiate the basis functions to the given order:
+        dn_u_dxn = [u_i.deriv(order) for u_i in self.u]
+        dn_dxn = np.zeros((self.N,self.N))
+        for i, u_i in enumerate(self.u):
+            for j, dn_u_j_dxn in enumerate(dn_u_dxn):
+                # Compute one matrix element <u_i | dn_dxn u_j> using the quadrature rule:
+                for x_n, w_n in zip(self.x, self.w):
+                    dn_dxn[i, j] += w_n*u_i(x_n)*dn_u_j_dxn(x_n)
+        return dn_dxn
 
 
 def gradientn(y, dx, n=1):
@@ -101,15 +128,17 @@ def gradientn(y, dx, n=1):
     return result
 
 
-def main():
+def test_single_element():
     N = 7
     differentiation_order = 1
 
     # Get our quadrature points and weights, our DVR basis functions, and
     # our differential operator in the DVR basis:
-    x, w = gauss_lobatto_points_and_weights(N)
-    u = make_DVR_basis(x, w)
-    dn_dxn = make_DVR_differetial_operator(x, w, order=differentiation_order)
+    dvr_basis = DVRBasis(N, -1, 1, 'lobatto')
+    x = dvr_basis.x
+    w = dvr_basis.w
+    u = dvr_basis.u
+    dn_dxn = dvr_basis.differential_operator(differentiation_order)
 
     # A dense grid for making plots
     x_dense = np.linspace(-1,1,1000)
@@ -118,15 +147,15 @@ def main():
     # Our Gaussian wavefunction, its representation in the DVR basis,
     # and that representation's interpolation back onto the dense grid:
     psi_dense = np.exp(-x_dense**2)
-    psi = make_DVR_vector(x_dense, psi_dense, x, w)
-    psi_interpolated = interpolate_DVR_vector(psi, x, w, x_dense)
+    psi = dvr_basis.make_vector(x_dense, psi_dense)
+    psi_interpolated = dvr_basis.interpolate_vector(psi, x_dense)
 
     # The derivative of our Gaussian wavefunction, the derivative of its
     # representation in the DVR basis (computed with the DVR derivative
     # operator), and that derivatives interpolation back onto the dense grid:
     d_psi_dense_dx = gradientn(psi_dense, dx, differentiation_order)
     d_psi_dx = np.dot(dn_dxn, psi)
-    d_psi_dx_interpolated = interpolate_DVR_vector(d_psi_dx, x, w, x_dense)
+    d_psi_dx_interpolated = dvr_basis.interpolate_vector(d_psi_dx, x_dense)
 
     # Plot the DVR basis functions:
     subplot(211)
@@ -158,4 +187,5 @@ def main():
 
     show()
 
-main()
+
+test_single_element()
